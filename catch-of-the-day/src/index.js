215fc0adad979ede8964b277255c9eaf8aebc8f4
SPachari <shankar.pachari@gmail.com>
2020-02-18 17:21:00 -0800

    first draft

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,1 +1,6 @@
-// let's go!
+import React from "react";
+import { render } from "react-dom";
+import App from "./components/App";
+import "./css/style.css";
+//main is the div id of the root tag in index.html.
+render(<App />, document.querySelector("#main"));
